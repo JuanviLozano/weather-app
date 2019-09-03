0e4efdd19d2eb5ff557208e983daa9ef8fa7993d
juan vicente lozano meca <juanvi.lozano96@gmail.com>
2019-09-03 22:58:05 +0200

    Constantes

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
+import {
+    CLOUD,
+    CLOUDY,
+    SUN,
+    RAIN,
+    SNOW,
+    WINDY,
+} from './../constants/Weathers';
 
 const WeatherData = () => (
     <div>
         <WeatherTemperature temperature={20} 
-                            weatherState={''}/>   
+                            weatherState={SUN}/>   
         <WeatherExtraInfo humidity={80} wind={"10 m/s"} />     
     </div>
 );
