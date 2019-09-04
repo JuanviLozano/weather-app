e0838a0ebee969cfe15036f72c149c37f52d4f44
juan vicente lozano meca <juanvi.lozano96@gmail.com>
2019-09-04 20:35:51 +0200

    Estructura de carpetas

--- src/components/WeatherLocation/Location.js
+++ src/components/WeatherLocation/Location.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Location = ({ city }) => (
     // Destructuring
@@ -6,5 +7,9 @@ const Location = ({ city }) => (
     // const city = props.city; == const { city } = props;
     <div><h1>{city}</h1></div>
 );
+
+Location.prototype = {
+    city: PropTypes.string.isRequired,
+};
 
 export default Location; 
